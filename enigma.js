abae1de1bfb6a90ae8800ce5eed86e2703dd85bc
Lion-El <68742917+Lion-El@users.noreply.github.com>
2023-11-14 09:09:09 +0000

    javascript code added for mouseover menu action

--- enigma.js
+++ enigma.js
@@ -1,1 +1,36 @@
-const subMenuLink = document.querySelector('.sub-menu-link');
+const ulChild = document.getElementsByClassName('menu-label');
+let menuName = [];
+
+for (let i=0; i < ulChild.length; i++) {
+    menuName.push(ulChild[i].children);
+}
+
+const mission = menuName[0];
+
+
+    mission.textContent.addEventlistener('mouseover', (e) => {
+        if (e.target === 'Mission') {
+            console.log(menuName[0]);
+        } else {
+            console.log('???');
+        };
+
+        if (e.target === 'Mission') {
+            console.log(menuName[1]);
+        } else {
+            console.log('???');
+        };
+
+        if (e.target === 'Mission') {
+            console.log(menuName[2]);
+        } else {
+            console.log('???');
+        };
+    });
+
+    const array = [1, 2, 3, 'you wish you could be me!'];
+
+    array.forEach((element) => console.log(element));
+
+    
+ 
